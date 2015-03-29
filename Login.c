be303c128db93643511c7d83fcdeaaf1f5f56b34
Kevin LUK <kluk6@linux.CS.McGill.CA>
2015-03-29 01:45:17 -0400

    initial commit

---
+++ Login.c
@@ -0,0 +1,14 @@
+#include <stdlib>
+#include <stdio>
+
+void main(void){
+	FILE *fp;
+	char c;
+	char file[8]="members"	
+	char *user=getenv("username");
+	char *pass=getenv("pass:);
+
+	fp=fopen(file, "rt");
+	
+		
+}
