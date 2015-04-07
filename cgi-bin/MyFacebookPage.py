79d675c3e73062e03714f84f3dbf9a1b6bc199bd
yasuin <nikolaus.yasui@mail.mcgill.ca>
2015-04-07 11:02:42 -0400

    Updated MyFacebookPage.py

--- cgi-bin/MyFacebookPage.py
+++ cgi-bin/MyFacebookPage.py
@@ -1,6 +1,8 @@
 from listing import memberList
 import AddFriend
 
+username=form.getvalue("username")
+
 memberlist = memberList()
 
 
@@ -18,18 +20,56 @@ print '''
 			<h1>
 				<b>World of Statistics</b>
 			</h1>
+
 			<font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font>
 		</center>
 	</header>
 
-	<table width="85%" align="center">
-		<tr>
-			<th> Member List </th>
-			<th> Username's Feed </th>
+	<form name="addfriend" action="MyFacebookPage.py" method="post">
+		<input type="hidden" name="username" value="
+'''
+print username
+print '''
+">
+		<table align="center">
+			<tr>	
+				<td align="justify">
+					<h5>Add friend:</h5>
+				</td>
+				<td align="justify">
+					<h5><input type="text" name="friend">
+					</h5></td>
+					<td align="justify"><input type="submit" value="submit">
+					</td>
+				</form>
+				<td>
+				&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+				</td>
+				<form name="poststatus" action="MyFacebookPage.py" method="post">
+				<input type="hidden" name="username" value="
+'''
+print username
+print '''
+">
+					<td align="justify"><h5>Name:</h5></td><td><h5><input type="text" name="status"></h5></td>
+					<td align="justify">
+						<input type="submit" value="submit">
+					</td>
+				</tr>
+			</table>
+		</form>
+
+		<table width="85%">
+			<tr>
+				<th> Member List </th>
+				<th align="justify"> 
+'''
+print username
+print ''''s Feed </th>
 		</tr>
 		<tr align="center">
 			<td>
-			'''
+'''
 for member in memberlist:
 	print member
 print '''
@@ -112,6 +152,8 @@ print '''
 		</tr>
 
 	</table>
+	
+	<p align="right"><font face="verdana" size="1"><a href="cs.mcgill.ca/~kluk6">LOGOUT</a>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;</font></p>
 
 	<footer>
 		<center>
