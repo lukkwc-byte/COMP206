029654e52a1cc73b6e544fee356623bc192b7551
Kevin LUK <kluk6@teaching.CS.McGill.CA>
2015-04-07 10:59:12 -0400

    adding feed logic

---
+++ cgi-bin/GetFeed.py
@@ -0,0 +1,25 @@
+def GetFeed():
+	Feedlist=[]
+	Users=[]
+	Messages=[]
+	with open("feed.csv") as feed:
+        	Feedlist=feed.read().splitlines()
+        	numLines=len(Feedlist)
+        	for i in range(len(Feedlist)):
+                	line=Feedlist[i]
+			if i % 2 == 0:
+				Users.append(line)
+			else:
+				Messages.append(line)
+		returnlist=[]
+		Users.reverse()
+		Messages.reverse()
+		if len(Users)>=10:
+			returnlist.append(Users[:10])
+			returnlist.append(Messages[:10])
+		else:
+			returnlist.append(Users)
+			returnlist.append(Messages)
+	return returnlist
+GetFeed()
+
