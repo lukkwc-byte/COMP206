737bac30b2459bf095e1d63e6fc4ad79bdb79f44
yasuin <nikolaus.yasui@mail.mcgill.ca>
2015-04-08 16:13:51 -0400

    testing ad2

--- cgi-bin/MyFacebookPage.py
+++ cgi-bin/MyFacebookPage.py
@@ -10,15 +10,11 @@ form = cgi.FieldStorage()
 username=form["username"].value
 
 if(os.getenv('HTTP_REFERER') == "http://cs.mcgill.ca/~kluk6/cgi-bin/MyFacebookPage.py"):
-	print "Content-Type: text/html"
-	print
-	print "<html><head></head><body>" + form["type"].value + "</body></html>"
 	if(form.getvalue("name") == "addfriend"):
-		x = 0/0
 		add(username, form.getvalue("friend"))
 		print "Content-Type: text/html"
 		print 
-		print "<html><head></head><body>"+form.getvalue("name")+"</body></html>"
+		print "<html><head></head><body>"+form["username"].value)+form.getvalue("friend")+"</body></html>"
 	if(form.getvalue("name") == "poststatus"):
 		writePost(username, form.getvalue("Post"))
 
