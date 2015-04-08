e291229dc512c18553154a2825761ffb072ea052
yasuin <nikolaus.yasui@mail.mcgill.ca>
2015-04-08 16:48:23 -0400

    aoeu

--- cgi-bin/Registration.pl
+++ cgi-bin/Registration.pl
@@ -25,24 +25,24 @@ $pwd =~ tr/ //ds;
 
 print "Content-type: text/html\n\n";
 
-my $file = $relative_path_to_csv;
-my $valid = 1;
+# my $file = $relative_path_to_csv;
+# my $valid = 1;
 
-open my $fh, "<", $file or die "$file: $!";
-while(my $line = <$fh>) {
-    chomp $line;
-    my @fields = split(" ", $line);
-    if($usr eq $fields[1]){
-        $valid = 0;
-        last;
-    }
-}
-close $fh;
+# open my $fh, "<", $file or die "$file: $!";
+# while(my $line = <$fh>) {
+#     chomp $line;
+#     my @fields = split(" ", $line);
+#     if($usr eq $fields[1]){
+#         $valid = 0;
+#         last;
+#     }
+# }
+# close $fh;
 
-if ($valid == 1){
-    open $fh, ">>", $file or die "$file: $!";
-    print $fh  "$name $usr $pwd\n";
-    close $fh;
+# if ($valid == 1){
+#     open $fh, ">>", $file or die "$file: $!";
+#     print $fh  "$name $usr $pwd\n";
+#     close $fh;
 
     print qq(
 <html>
@@ -80,55 +80,55 @@ if ($valid == 1){
 </html>
       );
 
-} else {
-    print qq(
- <html>
-<head>
-    <title> World of Statistics</title>
-</head>
+# } else {
+#     print qq(
+#  <html>
+# <head>
+#     <title> World of Statistics</title>
+# </head>
 
-<body bgcolor="#c4d4af">
-    <header>
-        <center>
-            <font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font>
-            <br>
-            <h1>
-                <b>World of Statistics</b>
-            </h1>
+# <body bgcolor="#c4d4af">
+#     <header>
+#         <center>
+#             <font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font>
+#             <br>
+#             <h1>
+#                 <b>World of Statistics</b>
+#             </h1>
 
-            <font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font>
-        </center>
-    </header>
+#             <font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font>
+#         </center>
+#     </header>
 
-    <section>
-        <center>
-            <p> Join the website that connects statistics enthusiasts together.</p>
-            <form name="register" action="cgi-bin/Registration.pl" method="post">
-                <br><br>
-                <h3><b>Sign up</b></h3>
-                <h6>Note: Spaces will be removed</h6>
-                <font color="red" style="font-family:verdana"><b>Your username must be unique. Please enter another userame.</b></font>
-                <table>
-                    <tr>    
-                        <td><h5>Name:</h5></td><td><h5><input type="text" name="name"></h5></td>
-                    </tr><tr>
-                    <td><h5>Username:</h5></td><td><h5><input type="text" name="username"></h5></td>
-                </tr><tr>
-                <td><h5>Password:</h5></td><td><h5><input type="text" name="password"></h5></td>
-            </tr>
-        </table>
-        <input type="submit" value="Register">
-    </form>
-</center>
-</section>
-    <footer>
-        <center>
-            <font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font><br>
-            <font size="2" face="verdana"><b>Made by: Team TimBitz</b></font><br>
-            <font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font>
-        </center>
-    </footer>
-</body>
-</html>
-        );
-}
+#     <section>
+#         <center>
+#             <p> Join the website that connects statistics enthusiasts together.</p>
+#             <form name="register" action="cgi-bin/Registration.pl" method="post">
+#                 <br><br>
+#                 <h3><b>Sign up</b></h3>
+#                 <h6>Note: Spaces will be removed</h6>
+#                 <font color="red" style="font-family:verdana"><b>Your username must be unique. Please enter another userame.</b></font>
+#                 <table>
+#                     <tr>    
+#                         <td><h5>Name:</h5></td><td><h5><input type="text" name="name"></h5></td>
+#                     </tr><tr>
+#                     <td><h5>Username:</h5></td><td><h5><input type="text" name="username"></h5></td>
+#                 </tr><tr>
+#                 <td><h5>Password:</h5></td><td><h5><input type="text" name="password"></h5></td>
+#             </tr>
+#         </table>
+#         <input type="submit" value="Register">
+#     </form>
+# </center>
+# </section>
+#     <footer>
+#         <center>
+#             <font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font><br>
+#             <font size="2" face="verdana"><b>Made by: Team TimBitz</b></font><br>
+#             <font color="#68a8ad" size="4">------------------------------------------------------------------------------------------------------------------------------------------------------------------</font>
+#         </center>
+#     </footer>
+# </body>
+# </html>
+#         );
+# }
