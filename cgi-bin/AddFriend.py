9950b79cb7f657c1ad4e507eed4518d110198da5
yasuin <nikolaus.yasui@mail.mcgill.ca>
2015-04-10 19:05:05 -0400

    please python

--- cgi-bin/AddFriend.py
+++ cgi-bin/AddFriend.py
@@ -3,16 +3,19 @@ def add(username, friend):
 	with open("members.csv") as members:
 		memberslist=members.read().splitlines()
 		valid = 1
+		j = -1
 		for i in range(len(memberslist)):
 			if(len(memberslist[i].split()) > 1):
 				if(username == memberslist[i].split()[1]):
+					j = i
 					if(len(memberslist[i].split()) > 3):
 						for usr in memberslist[i].split()[3:]:
 							if(friend == usr):
 								valid = 0
+								j = -1
 								break
-		if valid == 1:
-			memberslist[i] = memberslist[i] + " {0}".format(friend)
+		if valid == 1 and j > -1:
+			memberslist[j] = memberslist[j] + " {0}".format(friend)
 
 	members=open("members.csv", "w")	
 	for i in range(len(memberslist)):
