cb9b2f6172b75ea6746764e5b9d73bc449f48fd7
yasuin <nikolaus.yasui@mail.mcgill.ca>
2015-04-09 23:18:48 -0400

    go

--- cgi-bin/MyFacebookPage.py
+++ cgi-bin/MyFacebookPage.py
@@ -131,6 +131,8 @@ for i in range(max(len(memberlist), len(feedlist[0]))):
 		</tr>
 		'''.format(feedlist[0][i],feedlist[1][i])
 print '''
+	</table>
+	<table>
 	<tr>
 			<form action="MyFacebookPage.py" method="post">
 				<input type="hidden" name="username" value="{0}">
@@ -138,25 +140,24 @@ print '''
 				<td>
 				</td>
 				<td>
-				<h5>Add friend:</h5>
-				</td>
-				<td>
+					<h5>Add friend:</h5>
 					<h5><input type="text" name="friend"></h5>
-				</td>
-				<td>
-				<input type="submit" value="Add">
+					<input type="submit" value="Add">
 				</td>
 			</form>
-				<td>
-					<h5>What's on your mind?</h5>
+							<td>
+				</td>
+								<td>
+				</td>
+								<td>
+					
 				</td>
 			<form name="poststatus" action="MyFacebookPage.py" method="post">
 				<input type="hidden" name="type" value="poststatus">
 				<input type="hidden" name="username" value="{1}">
 				<td>
+					<h5>What's on your mind?</h5>
 					<h5><input type="text" name="status"></h5>
-				</td>
-				<td>
 					<input type="submit" value="Post">
 				</td>
 			</form>
