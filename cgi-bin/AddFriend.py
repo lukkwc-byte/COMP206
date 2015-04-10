3e2d9c1bae459db6d3ffcb0adc2a532cf0cf8006
yasuin <nikolaus.yasui@mail.mcgill.ca>
2015-04-10 13:04:05 -0400

    what am i even doing

--- cgi-bin/AddFriend.py
+++ cgi-bin/AddFriend.py
@@ -4,11 +4,10 @@ def add(username, friend):
 		memberslist=members.read().splitlines()
 
 		for i in range(len(memberslist)):
-			if(memberslist[i]):
-				if(lineUser = memberslist[i].split()[1]):
-					memberslist[i].split().append(" {0}".format(friend))
+			if(len(memberslist[i].split()) > 1):
+				if(username == memberslist[i].split()[1]):
+					memberslist[i] = memberslist[i] + " {0}".format(friend)
 
-	return listing
 	members=open("members.csv", "w")	
 	for i in range(len(memberslist)):
 		members.write(memberslist[i]+"\n")
